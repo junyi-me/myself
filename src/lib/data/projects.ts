b25ee94407485a5f6f8f2745ed99b3ef8ad02f9d
junyi.wang <junyi.wang.007@gmail.com>
2024-12-10 19:58:14 -0700

    fix broken link

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -3,8 +3,8 @@ import type { PProjectType } from "$lib/types";
 export const projects: PProjectType[] = [
   {
     txTitle: "projects.k3s.title",
-    img: "https://blog.junyi.me/p/hosting-an-https-website-on-a-ha-k3s-cluster/Https_Protocolo_hu7943313946152348111.jpg",
-    link: "https://blog.junyi.me/p/hosting-an-https-website-on-a-ha-k3s-cluster/",
+    img: "https://blog.junyi.me/p/hosting-an-https-website-on-a-ha-k3s-cluster-through-ssh/Https_Protocolo_hu7943313946152348111.jpg",
+    link: "https://blog.junyi.me/p/hosting-an-https-website-on-a-ha-k3s-cluster-through-ssh/",
   }
 ]
 
